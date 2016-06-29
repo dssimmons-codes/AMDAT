e9bb31053cd657358ca330ce12b8c87bf98410ad
dsimmon <dsimmon@25419694-f2fb-4e8e-af61-79680089b9de>
2016-06-29 00:56:37 +0000

    added files inadvertently excluded from prior commit
    git-svn-id: file:///home/pkawak/git/svn/tmp/AMDAT_trunk/AMDAT/trunk@109 25419694-f2fb-4e8e-af61-79680089b9de

---
+++ relative_displacement_strings.h
@@ -0,0 +1,41 @@
+/*Relative_Displacement_Strings class - Identifies particles remaining in spacial promity to one anotherand converts them to multibodies*/
+/*Amorphous Molecular dynamics analysis toolkit (AMDAT)*/
+/*Written by David S. Simmons*/
+
+
+#ifndef RELATIVE_DISPLACEMENT_STRINGS
+#define RELATIVE_DISPLACEMENT_STRINGS
+
+#include "multibody.h"
+#include "dynamic_cluster_multibodies.h"
+
+namespace std{
+  
+class Relative_Displacement_Strings: public Dynamic_Cluster_Multibodies
+{
+    float ** sigmatrix;		//stores particle sizes
+    int n_atomtypes;
+    
+    float neighbor_threshold;
+    float replacement_threshold;
+        
+    void allocate_sig_matrix(string);
+    bool clustered_check(Trajectory*, Trajectory*, int, int);
+    Coordinate get_imageoffset(Trajectory*, Trajectory*, int, int);
+  public:
+    
+    Relative_Displacement_Strings();
+    Relative_Displacement_Strings(const Relative_Displacement_Strings&);
+    ~Relative_Displacement_Strings();
+    Relative_Displacement_Strings operator=(const Relative_Displacement_Strings&);
+    
+    Relative_Displacement_Strings(System * syst, int tgap, float n_thresh, float r_thresh, string sigmatrixname);
+  
+};
+  
+  
+  
+}
+
+
+#endif
