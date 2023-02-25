45eabf11da73a7069bb783af34b3f15db1f81795
Pierre Kawak <b00039424@gmail.com>
2023-02-25 18:06:49 -0500

    fixes bug with overwriting analyze method and changes params to theta_cutoff and dist_cutoff

--- src/particles_between.h
+++ src/particles_between.h
@@ -18,31 +18,30 @@ namespace std{
 
 class Particles_Between: public Trajectory_List, public Analysis_Onetime
 {
-  float maxdistance;    //I'm guessing we need some variable like this to store the max total distance between the two secondary particles
-  float radius;         //I'm guessing we need a second variable for that lateral distance. 
-  
+//  float maxdistance;    //I'm guessing we need some variable like this to store the max total distance between the two secondary particles
+  float dist_cutoff;         //I'm guessing we need a second variable for that lateral distance.
+  float theta_cutoff;
+
   public:
     Particles_Between();
     Particles_Between(System*);
     Particles_Between(System*,float,float);  //put other required arguments in this one
     Particles_Between(const Particles_Between &);
     ~Particles_Between();
-  
+
     Particles_Between operator = (const Particles_Between &);	//assignment
-  
+
     Analysis_Type what_are_you(){Analysis_Type type = particles_between; return type;};   //this analysis type needs to be added at top of ananlysis parent class
-  
-    void preprocess();    //this is where any preprocessing prior to loops should live 
-    void preprocess2();    //this is where any preprocessing prior to loops should live 
+
+    void preprocess();    //this is where any preprocessing prior to loops should live
+    void preprocess2();    //this is where any preprocessing prior to loops should live
     void timekernel(int timeii){timekernel2(timeii);};
     void timekernel2(int timeii);
     void listkernel(Trajectory *, int, int, int);
     void listkernel2(Trajectory *, Trajectory *, int, int, int);
-    void analyze (Trajectory_List *);
-    void analyze (Trajectory_List *, Trajectory_List*);
     void bin_hook(Trajectory_List *,int,int,int);
     void postprocess_bins();
-  
+
 };
 
 }
