a3d8e27ed241bffd0194d7573e17da67ffa3e751
Pierre Kawak <b00039424@gmail.com>
2023-02-25 18:06:49 -0500

    fixes bug with overwriting analyze method and changes params to theta_cutoff and dist_cutoff

--- src/particles_between.cpp
+++ src/particles_between.cpp
@@ -32,7 +32,7 @@ Particles_Between::Particles_Between(System * syst)
 
   sys=syst;
   system = const_cast<System*>(sys);
-   
+
   capacity=system->show_n_atoms()+system->show_n_molecules();   //This sets how much memory will be allocated for trajectory list at each time. This is just an estimate.
   n_times = system->show_n_timesteps();     //sets number of times for memory allocation to equal number of times in system
 
@@ -47,13 +47,13 @@ Particles_Between::Particles_Between(System * syst)
   }
 }
 
-Particles_Between::Particles_Between(System * syst, float maxdist, float rad)
+Particles_Between::Particles_Between(System * syst, float d_cutoff, float t_cutoff)
 {
   int timeii;
 
   sys=syst;
   system = const_cast<System*>(sys);
-   
+
   capacity=system->show_n_atoms()+system->show_n_molecules();   //This sets how much memory will be allocated for trajectory list at each time. This is just an estimate.
   n_times = system->show_n_timesteps();     //sets number of times for memory allocation to equal number of times in system
 
@@ -66,8 +66,8 @@ Particles_Between::Particles_Between(System * syst, float maxdist, float rad)
     n_trajectories[timeii]=0;
     included[timeii].set(sys);
   }
-  maxdistance=maxdist;
-  radius=rad;
+  dist_cutoff=d_cutoff;
+  theta_cutoff=t_cutoff;
 }
 
 
@@ -75,9 +75,9 @@ Particles_Between::Particles_Between(const Particles_Between & copy):Trajectory_
 {
 //  Trajectory_List::Trajectory_List(copy);
 //  Analysis::Analysis(copy);
-  maxdistance=copy.maxdistance;
-  radius=copy.radius;
-  
+  dist_cutoff=copy.dist_cutoff;
+  theta_cutoff=copy.theta_cutoff;
+
 }
 
 Particles_Between::~Particles_Between()
@@ -100,14 +100,14 @@ Particles_Between Particles_Between::operator = (const Particles_Between & copy)
 
   n_times = system->show_n_timesteps();     //sets number of times for memory allocation to equal number of times in system
 
-  maxdistance=copy.maxdistance;
-  radius=copy.radius;
+  dist_cutoff=copy.dist_cutoff;
+  theta_cutoff=copy.theta_cutoff;
 
 }
 
 void Particles_Between::timekernel2(int timeii)
 {
-   trajectory_list->listloop(this,0, timeii, 0);  
+   trajectory_list->listloop(this,0, timeii, 0);
 }
 
 
@@ -119,36 +119,37 @@ void Particles_Between::listkernel(Trajectory* current_trajectory, int timegapii
 
 void Particles_Between::listkernel2(Trajectory* traj1, Trajectory* traj2,int timegapii,int thisii, int nextii) //need to check back on which of these time indices is the right one.
 {
-   Trajectory* traj3;   
-   bool isbetween;    //boolean to store whether the particle is in the 'between' set at this time
-   
-   int n_trajs=trajectory_list2->show_n_trajectories(thisii);  //record how many particles are in the second trajectory list at this time
-   
-   for(int trajii=0;trajii<n_trajs;trajii++)  //here is the second loop over particles
-   {
-      traj3 = (*trajectory_list2)(thisii,trajii);  //this is the syntax to pull the second particle in set 2
-      //we now have pointers to all 3 particles here.
-      //traj1 is the central particle you are trying to sort
-      //traj2 is the first particle in the group you are looking between
-      //traj3 is the second particle in the group you are looking between
-         
-      //do some math here to determine whether particle is in between
-      //if it is, set isbetween to true.
-      //if it is not, set isbetween to false
-      
-         if(isbetween)
-         {
-            addtrajectory(thisii,traj1);        //this line will add the trajectory to the trajectory list
-         }
-   }
-      
+//  cout << "Inside listkernel2. Time: " << thisii << " ids: " << traj1->show_trajectory_ID() << " " << traj2->show_trajectory_ID() << "\n";
+  Trajectory* traj3;
+  bool isbetween = false;    //boolean to store whether the particle is in the 'between' set at this time
+
+  int n_trajs=trajectory_list2->show_n_trajectories(thisii);  //record how many particles are in the second trajectory list at this time
+
+  for(int trajii=0;trajii<n_trajs;trajii++)  //here is the second loop over particles
+  {
+    traj3 = (*trajectory_list2)(thisii,trajii);  //this is the syntax to pull the second particle in set 2
+    //we now have pointers to all 3 particles here.
+    //traj1 is the central particle you are trying to sort
+    //traj2 is the first particle in the group you are looking between
+    //traj3 is the second particle in the group you are looking between
+
+    //do some math here to determine whether particle is in between
+    //if it is, set isbetween to true.
+    //if it is not, set isbetween to false
+
+    if(isbetween)
+    {
+       addtrajectory(thisii,traj1);        //this line will add the trajectory to the trajectory list
+    }
+  }
+
   float distance;
   if(traj2!=traj3)
   {
     distance=(traj3->show_coordinate(thisii)-(traj2->show_coordinate(thisii))).length_unwrapped(system->size());	//calculate shortest distance between two coordinates, taking into account periodic boundaries
 //    bin(thisii,distance);
   }
-   
+
 }
 
 void Particles_Between::preprocess()
@@ -156,14 +157,6 @@ void Particles_Between::preprocess()
 }
 
 void Particles_Between::preprocess2()
-{
-}
-
-void Particles_Between::analyze (Trajectory_List *)
-{
-}
-
-void Particles_Between::analyze (Trajectory_List *, Trajectory_List*)
 {
 }
 
@@ -175,4 +168,3 @@ void Particles_Between::postprocess_bins()
 {
   //I don't think any postprocessing is needed in this case
 }
-
